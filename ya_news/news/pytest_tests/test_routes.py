5f523cc0a61287f94d45f2e96f43b65d91025a50
Stepan Dilman <stepan.dilman@gmail.com>
2025-01-18 04:24:04 +0500

    review-3: pytest

--- ya_news/news/pytest_tests/test_routes.py
+++ ya_news/news/pytest_tests/test_routes.py
@@ -16,30 +16,24 @@ COMMENT_EDIT_URL = lazy_fixture("comment_edit_url")
 COMMENT_EDIT_REDIRECT_URL = lazy_fixture("comment_edit_redirect_url")
 COMMENT_DELETE_URL = lazy_fixture("comment_delete_url")
 COMMENT_DELETE_REDIRECT_URL = lazy_fixture("comment_delete_redirect_url")
-
+ANONIMOUS_CLIENT = lazy_fixture("client")
+AUTHOR_CLIENT = lazy_fixture("author_client")
+READER_CLIENT = lazy_fixture("reader_client")
 
 @mark.parametrize(
     "url, test_client, status_code",
     (
-        (NEWS_HOME_URL, lazy_fixture("client"), HTTPStatus.OK),
-        (NEWS_URL, lazy_fixture("client"), HTTPStatus.OK),
-        (LOGIN_URL, lazy_fixture("client"), HTTPStatus.OK),
-        (LOGOUT_URL, lazy_fixture("client"), HTTPStatus.OK),
-        (SIGNUP_URL, lazy_fixture("client"), HTTPStatus.OK),
-        (COMMENT_EDIT_URL, lazy_fixture("author_client"), HTTPStatus.OK),
-        (
-            COMMENT_EDIT_URL,
-            lazy_fixture("reader_client"),
-            HTTPStatus.NOT_FOUND
-        ),
-        (COMMENT_EDIT_URL, lazy_fixture("client"), HTTPStatus.FOUND),
-        (COMMENT_DELETE_URL, lazy_fixture("author_client"), HTTPStatus.OK),
-        (
-            COMMENT_DELETE_URL,
-            lazy_fixture("reader_client"),
-            HTTPStatus.NOT_FOUND
-        ),
-        (COMMENT_DELETE_URL, lazy_fixture("client"), HTTPStatus.FOUND),
+        (NEWS_HOME_URL, ANONIMOUS_CLIENT, HTTPStatus.OK),
+        (NEWS_URL, ANONIMOUS_CLIENT, HTTPStatus.OK),
+        (LOGIN_URL, ANONIMOUS_CLIENT, HTTPStatus.OK),
+        (LOGOUT_URL, ANONIMOUS_CLIENT, HTTPStatus.OK),
+        (SIGNUP_URL, ANONIMOUS_CLIENT, HTTPStatus.OK),
+        (COMMENT_EDIT_URL, AUTHOR_CLIENT, HTTPStatus.OK),
+        (COMMENT_EDIT_URL, READER_CLIENT, HTTPStatus.NOT_FOUND),
+        (COMMENT_EDIT_URL, ANONIMOUS_CLIENT, HTTPStatus.FOUND),
+        (COMMENT_DELETE_URL, AUTHOR_CLIENT, HTTPStatus.OK),
+        (COMMENT_DELETE_URL, READER_CLIENT, HTTPStatus.NOT_FOUND),
+        (COMMENT_DELETE_URL, ANONIMOUS_CLIENT, HTTPStatus.FOUND)
     ),
 )
 def test_status_code(url, test_client, status_code):
